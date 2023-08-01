81488f8e5fa6633d2321beb2636599439ac6120d
Won Lee <whd793@gmail.com>
2023-08-01 16:17:43 +0900

    MERN first commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import Header from './components/Header';
+import Public from './components/Public';
+import NotFound from './components/NotFound';
+import UserSignUp from './components/UserSignUp';
+import UserSignIn from './components/UserSignIn';
+import UserSignOut from './components/UserSignOut';
+import Authenticated from './components/Authenticated';
+
+export default () => (
+  <Router>
+    <div>
+      <Header />
+
+      <Switch>
+        <Route exact path="/" component={Public} />
+        <Route path="/authenticated" component={Authenticated} />
+        <Route path="/signin" component={UserSignIn} />
+        <Route path="/signup" component={UserSignUp} />
+        <Route path="/signout" component={UserSignOut} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
